1ae2e7e91644baea7f391891e28278fbadf5747e
Lawftyg <mymsnadressis@hotmail.com>
2024-08-15 22:45:47 +0200

    fix mistype in shader_support

--- html5_canvas/src/shader_support.js
+++ html5_canvas/src/shader_support.js
@@ -2,36 +2,40 @@
 import * as core from "./core.js";
 import * as vertexBuffer from "./vertex_buffer.js";
 
-let compiledShader = null;
+let mCompiledShader = null;
 let vertexPositionRef = null;
 
 function loadAndCompileShader(id, shaderType) {
-  let shaderSource = null;
-  let intCompiledShader = null;
+  let shaderSource = null,
+    compiledShader = null;
 
   // Step A: Get the shader source from index.html
   let shaderText = document.getElementById(id);
-  shaderSource = shaderText.firstChild.textContext;
+  shaderSource = shaderText.firstChild.textContent;
 
   let gl = core.getGL();
   // Step B: Create shader based on type: vertex of fragment
-  intCompiledShader = gl.createShader(shaderType);
+  compiledShader = gl.createShader(shaderType);
 
   // Step C: Compile the created shader
-  gl.shaderSource(intCompiledShader, shaderSource);
-  gl.compileShader(intCompiledShader);
+  gl.shaderSource(compiledShader, shaderSource);
+  gl.compileShader(compiledShader);
 
   // Step D: check for errors and return results (null if error)
   // The log info is how shader compilation errors are displayed.
   // This is usefull for debugging the shaders.
-  if (!gl.getShaderParameter(intCompiledShader, gl.COMPILE_STATUS)) {
+  if (!gl.getShaderParameter(compiledShader, gl.COMPILE_STATUS)) {
     throw new Error(
-      "A shader compiling error occured: " +
-        gl.getShaderInfoLog(intCompiledShader)
+      "A shader compiling error occured:\ntype:" +
+        shaderType +
+        "\nid: " +
+        id +
+        "\n" +
+        gl.getShaderInfoLog(compiledShader)
     );
   }
 
-  return intCompiledShader;
+  return compiledShader;
 }
 
 function init(vertexShaderID, fragmentShaderID) {
@@ -45,18 +49,18 @@ function init(vertexShaderID, fragmentShaderID) {
   );
 
   // Step B: Create and link the shaders into a program
-  compiledShader = gl.createProgram();
-  gl.attachShader(compiledShader, vertexShader);
-  gl.attachShader(compiledShader, fragmentShader);
-  gl.linkProgram(compiledShader);
+  mCompiledShader = gl.createProgram();
+  gl.attachShader(mCompiledShader, vertexShader);
+  gl.attachShader(mCompiledShader, fragmentShader);
+  gl.linkProgram(mCompiledShader);
 
   // Step C: check for error
-  if (!gl.getProgramParameter(compiledShader, gl.LINK_STATUS)) {
+  if (!gl.getProgramParameter(mCompiledShader, gl.LINK_STATUS)) {
     throw new Error("Error linking shader");
   }
 
   // Step D: Gets reference to aVertexPosition attribute in the shader
-  vertexPositionRef = gl.getAttribLocation(compiledShader, "aVertexPosition");
+  vertexPositionRef = gl.getAttribLocation(mCompiledShader, "aVertexPosition");
 }
 
 function activate() {
@@ -64,7 +68,7 @@ function activate() {
   let gl = core.getGL();
 
   // Step B: identify the compiled shader to use
-  gl.useProgram(compiledShader);
+  gl.useProgram(mCompiledShader);
 
   // Step C: bind vertex buffer to attribute defined in vertex shader
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer.get());
